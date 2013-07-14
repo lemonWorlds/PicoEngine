2dd9bf24fe4545aec9883762e8ddfb41e963bc8c
tohaga01 <tohaga01@cspc546.dcs.bbk.ac.uk>
2013-07-14 21:30:49 +0100

    Commit for end of eve.

--- src/main/SimpleActionScheduler.java
+++ src/main/SimpleActionScheduler.java
@@ -3,9 +3,19 @@ package main;
 import interfaces.ActionScheduler;
 
 public class SimpleActionScheduler implements ActionScheduler {
-
+	private EventHandler handler = null;
+	
+	public SimpleActionScheduler(EventHandler handler) {
+		this.handler = handler;
+	}
+	
 	@Override
 	public void scheduleAction(ECARule rule) {
+		String action = rule.getAction();
+		if (!action.equals("Update")) {
+			ECAEvent event = new SimpleECAEvent(action);
+			handler
+		}
 		System.out.println("Action: " + rule.getAction());
 	}
 
